af516cfba2f2d2f212b0264211bc6300d6debc4d
mllesta <122868035+mariollesta@users.noreply.github.com>
2025-07-25 11:02:37 +0200

    add first version of the portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
